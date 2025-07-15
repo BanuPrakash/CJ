6a9f0daf10d1bc634a84c8749eee873b4802123c
Banuprakash <banuprakashc@yahoo.co.in>
2025-07-15 12:25:40 +0530

    ORM

---
+++ workspace/orderapp/src/main/java/com/cisco/orderapp/entity/Product.java
@@ -0,0 +1,21 @@
+package com.cisco.orderapp.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+@Entity
+@Table(name="products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private double price;
+}
